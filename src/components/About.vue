15043d5b7e468779ce7e4ca41fbb5c4ae0c4d88e
Clare <chblackburne@gmail.com>
2018-11-10 17:15:48 +0000

    Example of how to get values from params

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="about">
     <h1>About Page</h1>
+    <p>Hello {{ visitor_name }}!</p>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali...</p>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      visitor_name: this.$route.params.name
+    }
+  }
+}
+</script>
