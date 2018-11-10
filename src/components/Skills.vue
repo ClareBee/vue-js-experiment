e9f8694821ad00c6e64deb6228d8ecbff73c4bf6
Clare <chblackburne@gmail.com>
2018-11-10 15:50:27 +0000

    Adds scoped external stylesheet

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -27,20 +27,6 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<!-- "scoped" attribute limits CSS to this component only -->
+<style src="./Skills.css" scoped>
 </style>
